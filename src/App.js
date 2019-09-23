00e12e6168a1f9bc3df82166065e85342f0e9e40
Evan Miller <emiller.arkko@gmail.com>
2019-09-23 21:52:33 +0300

    Initial prototype, first commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,125 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentQuestionId: 1,
+      questions: [
+          {
+              id: 1,
+              question: "Haluatko lukea kirjaa?",
+              yes: 2,
+              no: 3
+          },
+          {
+              id:2,
+              question: "Kun olet lukenut kirjan, juoksetko?",
+              yes: 4,
+              no: 5
+          },
+          {
+              id:3,
+              question: "Etkö osaa lukea?",
+              yes: 6,
+              no: 7
+          },
+          {
+              id:4,
+              question: "Juoksetko kauas?",
+              yes: 8,
+              no: 1
+          },
+          {
+              id:5,
+              question: "Miksi et juokse",
+              yes: 1,
+              no: 1
+          },
+          {
+              id:6,
+              question: "opetatko minutkin sitten lukemaan?",
+              yes: 1,
+              no: 1
+          },
+          {
+              id:7,
+              question: "Haluatko opetella lukemaan?",
+              yes: 1,
+              no: 1
+          },
+          {
+              id:8,
+              question: "En usko",
+              yes: 1,
+              no: 1
+          },
+      ]
+    }
+  }
+    
+    componentWillMount() {
+        document.addEventListener("keydown", this.onKeyPressed.bind(this));
+    }
+    
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.onKeyPressed.bind(this));
+    }
+    
+    onKeyPressed(e) {
+        console.log(e.keyCode);
+        switch (e.keyCode) {
+            case 65:
+                console.log("yes")
+                this.yes()
+                break
+            case 76:
+              this.no()
+                console.log("no");
+                break;
+            
+                
+        }
+    }
+    
+    yes = () => {
+        const question = this.state.questions.filter(q => q.id === this.state.currentQuestionId)[0]
+        this.setState({currentQuestionId: this.state.questions.map(q => q.id).filter(id => id === question.yes)[0]})
+    }
+    no = () => {
+        const question = this.state.questions.filter(q => q.id === this.state.currentQuestionId)[0]
+        this.setState({currentQuestionId: this.state.questions.map(q => q.id).filter(id => id === question.no)[0]})
+    }
+    
+  
   render() {
+    const question = this.state.questions.filter(q => q.id === this.state.currentQuestionId)[0]
+    
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>{this.state.currentQuestionId === 1 ? "GORS" : "????"}</h1>
+          <span style={{flexDirection: "row", marginBottom: 20}}>
+              <span>{question.question}</span>
+          </span>
+          <span>
+            <span
+                onClick={this.yes}
+                style={{...styles.answer, color: "lightgreen"}}>KYLLÄ</span>
+            <span
+                onClick={this.no}
+                style={{...styles.answer, color: "red"}}>EI</span>
+          </span>
         </header>
       </div>
     );
+  }
+}
+
+const styles = {
+  answer: {
+      margin: 25,
+      fontWeight: "bold",
   }
 }
 
