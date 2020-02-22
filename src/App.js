985d1eb8ab8a25005e6a2781c0ecca05a21f8b82
Evan Miller <emiller.arkko@gmail.com>
2020-02-22 12:49:04 +0100

    Add functionality for question with no answers and question with no questiontext, start writing out story

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import tausta from "./gors_salt_sack.JPG"
-import startScreen1 from "./bg1.JPG"
-import startScreen2 from "./bg2.JPG"
-import startScreen3 from "./bg3.JPG"
-import startScreen4 from "./bg4.JPG"
+import saltSackImage from "./gors_salt_sack.JPG"
+import backgroundImage from "./gors_bg.jpeg"
 
 import './App.css';
 
@@ -13,66 +10,99 @@ class App extends Component {
         super();
         this.state = {
             startScreenIndex: 1,
-            gameStarted: false,
+            gameStarted: true,
             currentQuestionId: 1,
             questions: [
                 {
                     id: 1,
-                    question: "Haluatko lukea kirjaa, joka kertoo jostain, mikä ei kiinnosta sinua yhtään?",
+                    question: "ENTER THE WORLD",
                     yes: 2,
-                    no: 3,
-                    yesText: "Kyl vitus",
-                    noText: "Ei vitus"
+                    no: 2,
+                    yesText: "YES",
+                    noText: "WOHOO"
                 },
                 {
                     id: 2,
-                    question: "Kun olet lukenut kirjan, juoksetko?",
-                    yes: 4,
-                    no: 5
+                    question: "Go look for answers",
+                    yes: 3,
+                    no: null,
+                    yesText: "LEFT",
+                    noText: "RIGHT"
                 },
                 {
                     id: 3,
-                    question: "Etkö osaa lukea?",
-                    yes: 6,
-                    no: 7
+                    yes: 4,
+                    no: 8,
+                    yesText: "Check the forest",
+                    noText: "Look into the fountain"
                 },
                 {
                     id: 4,
-                    question: "Juoksetko kauas?",
-                    yes: 8,
-                    no: 1
+                    question: "THUNDER STRIKES",
+                    yes: 5,
+                    no: 5,
+                    yesText: "Run to safety",
+                    noText: "Embrace the thunder"
                 },
                 {
                     id: 5,
-                    question: "Miksi et juokse",
-                    yes: 1,
-                    no: 1
+                    question: "HUNGER STRIKES",
+                    yes: 6,
+                    no: null,
+                    yesText: "Eat the frog",
+                    noText: "Keep on looking"
                 },
                 {
                     id: 6,
-                    question: "opetatko minutkin sitten lukemaan?",
-                    yes: 1,
-                    no: 1
+                    yes: 7,
+                    no: null,
+                    yesText: "Help Thalamis",
+                    noText: "Search the saltbag from iglu"
                 },
                 {
                     id: 7,
-                    question: "Haluatko opetella lukemaan?",
+                    question: "YOU FOUND AN ENDING\n" +
+                    "White blids: x,\n" +
+                    "lost and found: route, frogfriend, empowerement, poison, kombo = gloves + fungi",
                     yes: 1,
-                    no: 1
+                    no: 1,
+                    yesText: "OK"
                 },
+                
                 {
                     id: 8,
-                    question: "En usko",
-                    yes: 1,
-                    no: 1
+                    question: "You found an ancient terasure but you need a key to open it",
+                    yes: 9
                 },
+                {
+                    id: 9,
+                    yes: 10,
+                    no: null,
+                    yesText: "JUMPS AND TURNS",
+                    noText: "SWING AND FLY"
+                },
+                {
+                    id: 10,
+                    yes: 11,
+                    no: null,
+                    yesText: "VENTURE THE ENDLESS DESERT",
+                    noText: "STICK TOGETHER"
+                },
+                {
+                    id: 11,
+                    question: "YOU FOUND AN ENDING\n" +
+                    "White blids: x,\n" +
+                    "lost and found: route, hyper vision, rolling spin, body care, separation",
+                    yes: 1,
+                    no: 1,
+                    yesText: "OK"
+                }
             ]
         }
     }
     
     componentWillMount() {
         document.addEventListener("keydown", this.onKeyPressed.bind(this));
-        this.rotateScreen()
     }
     
     componentWillUnmount() {
@@ -110,40 +140,50 @@ class App extends Component {
     
     yes = () => {
         const question = this.state.questions.filter(q => q.id === this.state.currentQuestionId)[0]
-        this.setState({currentQuestionId: this.state.questions.map(q => q.id).filter(id => id === question.yes)[0]})
+        if(question.yes){
+            this.setState({currentQuestionId: this.state.questions.map(q => q.id).filter(id => id === question.yes)[0]})
+        }
     }
     no = () => {
         const question = this.state.questions.filter(q => q.id === this.state.currentQuestionId)[0]
-        this.setState({currentQuestionId: this.state.questions.map(q => q.id).filter(id => id === question.no)[0]})
+        if(question.no){
+            this.setState({currentQuestionId: this.state.questions.map(q => q.id).filter(id => id === question.no)[0]})
+        }
     }
     
     
     render() {
         const question = this.state.questions.filter(q => q.id === this.state.currentQuestionId)[0]
+        if(question && !question.yesText) {
+            setTimeout(() => this.yes(), 5000);
+        }
         
         return (
             <div className="App">
                 <header className="App-header">
                     {this.state.gameStarted ?
                         <React.Fragment>
-                            <img src={tausta} alt="TAUSTA" className="background-image"/>
+                            <img src={backgroundImage} alt="TAUSTA" className="background-image"/>
                             <div className="app-content">
-                                <h1 className="title">{question.question}</h1>
+                                {question.question &&
+                                <h1 className="title">{question.question}</h1>}
                                 <span>
-            <span
-                className="button"
-                onClick={this.yes}
-                style={{...styles.answer, color: "#00de3b"}}>KYLLÄ</span>
-            <span
-                className="button"
-                onClick={this.no}
-                style={{...styles.answer, color: "#ff1414"}}>EI</span>
+                                    {question.yesText &&
+                                    <span
+                                        className="button"
+                                        onClick={this.yes}
+                                        style={{...styles.answer, color: "#00de3b"}}>{question.yesText}</span>}
+                                    {question.noText &&
+                                    <span
+                                        className="button"
+                                        onClick={this.no}
+                                        style={{...styles.answer, color: "#ff1414"}}>{question.noText}</span>}
           </span>
                             </div>
                         </React.Fragment> :
                         <React.Fragment>
                             <img
-                                src={[startScreen1, startScreen2, startScreen3, startScreen4][this.state.startScreenIndex]}
+                                src={saltSackImage}
                                 alt="TAUSTA" className="background-image"/>
                             <div className="app-content">
                                 <h3 className="blinking">PRESS ANY BUTTON TO START GAME...</h3>
