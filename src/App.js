8987d0a8e1c6b9c15a39b2a75d78b602b50191bf
Evan Miller <emiller.arkko@gmail.com>
2020-02-23 11:13:28 +0100

    gamestarted = false

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         super();
         this.state = {
             startScreenIndex: 1,
-            gameStarted: true,
+            gameStarted: false,
             currentQuestionId: 1,
             isAutoleftTimerRunning: false
         }
